d56b8a54a0df25475a666059def87c5bf2e76ea4
Jack <90474780+lemonadepsd@users.noreply.github.com>
2021-10-15 13:53:05 -0400

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,6 @@
-#Python Webscraper
-
 import sys
 import requests
-
+import nltk
 import tweepy
 from english_words import english_words_set
 from bs4 import BeautifulSoup
@@ -19,39 +17,53 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 URL = "https://www.w3schools.com/python/python_file_open.asp"
 page = requests.get(URL)
 
+filename = "common_words.txt"
+words_file = open(filename, 'r')
+most_common_words = [line.split('\n')[0] for line in words_file.readlines()]
 
 words_and_frequencies = {}
 
 soup = BeautifulSoup(page.content, 'html.parser')
 paragraph_text = soup.find_all("p")
 
+def remove_last_letter(w):
+  try:
+    if not w[-1].isalpha():
+      w=w[:-1]
+      w=remove_last_letter(w)
+    return w
+  except:
+    return w
+
 for line in paragraph_text:
-  for word in paragraph_text.text.strip().split():
-    if not word.lower()[-1].isalpha():
-      word=word[:-1]
-    print(word)
-    if word.lower() in english_words_set:
-      print(word)
-""" for p in paragraph_text:
-  for word in p.text.strip().split():
-    if word.lower() in words_and_frequencies:
-      words_and_frequencies[word.lower()] += 1
-    elif word.lower() in english_words_set:
-      words_and_frequencies[word.lower()] = 1 """
+  for word in line.text.strip().split():
+    word=word.lower()
+    word = remove_last_letter(word)
+    if word in words_and_frequencies:
+      words_and_frequencies[word] += 1
+    elif word in english_words_set:
+      words_and_frequencies[word] = 1
 
-""" words = list(words_and_frequencies.keys())
+for word_check in most_common_words:
+  if word_check in words_and_frequencies.keys():
+    words_and_frequencies.pop(word_check)
+
+
+words = list(words_and_frequencies.keys())
 frequencies = list(words_and_frequencies.values())
+print(words)
+print(frequencies)
 
-print(words_and_frequencies)
+
 
 textfile = open("words.txt", "w")
 for element in words:
     textfile.write(element + "\n")
 textfile.close()
-
 textfile = open("frequencies.txt", "w")
 for element in frequencies:
     textfile.write(str(element) + "\n")
 textfile.close()
+print("completed")
 
-print("completed") """
+
