1366d5bddf17f7ad0b9f8c3eb9383411b299a741
Ann <92816811+annhaas@users.noreply.github.com>
2021-10-21 14:00:06 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,25 +1,23 @@
 anychart.onDocumentReady(function() { 
-   var fs = require('fs');
-   var text = fs.readFileSync("words.txt", 'utf-8');
-   var words = text.split('\n');
-
-   var fs = require('fs');
-   var text = fs.readFileSync("numbers.txt", 'utf-8');
-   var values = text.split('\n');
-
+ var dict = {"ann":100,
+"Courtenay" : 6,
+"leighty": 8};
 
 
  var table = [];
- for(var i = 0; i < words.length; i++){
-    table.push({"x": words[i], "value": values[i]});
+ for(var key in dict){
+    var value = dict[key];
+    console.log(value);
+    table.push({"x": key, "value": value});
+
  }
-    
+ 
+
     var chart = anychart.tagCloud(table);
     
     chart.title('most common words in an article');
     chart.angles([0, 15, -15, 30, -30]);
-    chart.colorRange(true);
-    chart.colorRange().length('80%');
+    
     
     chart.container("container");
     chart.draw();
