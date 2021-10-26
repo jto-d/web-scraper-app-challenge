f64ea34b55aa074c35c09fcbddaa8993b7b403d5
Jack <90474780+lemonadepsd@users.noreply.github.com>
2021-10-26 00:44:06 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import sys
 import requests
-from english_words import english_words_set
-from bs4 import BeautifulSoup
-import json
 
 BASE = "http://127.0.0.1:5000/"
 
-
-
-response = requests.get(BASE + "webpage/yes")
+response = requests.get(BASE + "webpage/yes" )
 print(response.json())
